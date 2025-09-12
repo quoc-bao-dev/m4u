0620f8750cba03e27e8998595d91511ba06a7525
Quoc Bao <quocbaodev1102@gmail.com>
2025-09-12 21:57:48 +0700

    Implement loading state in ProductSection and minor adjustments in FilterMobile
    - Added loading state handling in ProductSection to display skeleton loaders while fetching product data.
    - Updated FilterMobile component to improve code readability by removing unnecessary class names.

--- src/modules/trial-registration/components/product/ProductSection.tsx
+++ src/modules/trial-registration/components/product/ProductSection.tsx
@@ -8,9 +8,10 @@ import FilterMobile from '../filters/FilterMobile'
 import FilterSidebar from './FilterSidebar'
 import ProductCard from './ProductCard'
 import ScrollRevealCard from './ScrollRevealCard'
+import { Skeleton } from '@/components/ui/skeleton'
 
 const ProductSection = () => {
-  const { data: productList } = useGetProductList()
+  const { data: productList, isLoading } = useGetProductList()
 
   console.log('[productList]', productList)
 
@@ -51,27 +52,50 @@ const ProductSection = () => {
             </div>
             <div className="flex-1">
               <div className="grid grid-cols-1  md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4  gap-5">
-                {products.map((p, index) => (
-                  <ScrollRevealCard
-                    key={p.id}
-                    delay={index * 0.1} // Stagger animation for each card
-                    duration={0.6}
-                  >
-                    <Link href={`/product/${p.slug}`}>
-                      <ProductCard
-                        brand={p.brand}
-                        productName={p.productName}
-                        participation={p.participation}
-                        image={p.image}
-                        imageAlt={p.imageAlt}
-                        rate={p.rate}
-                        bgColor={p.bgColor!}
-                        hex={p.hex!}
-                        time={p.time!}
-                      />
-                    </Link>
-                  </ScrollRevealCard>
-                ))}
+                {isLoading
+                  ? Array.from({ length: 8 }).map((_, index) => (
+                      <div key={index} className="bg-gray-50- rounded-3xl">
+                        <div className="relative shadow-[0px_4px_24px_0px_#0000000F] rounded-3xl h-fit select-none w-full">
+                          <div className="rounded-t-3xl relative overflow-hidden w-full">
+                            <div className="absolute top-4 left-4 flex items-center gap-1 bg-white rounded-full py-0.5 px-1.5 text-base font-medium">
+                              <Skeleton className="h-5 w-16" />
+                            </div>
+                            <Skeleton className="w-full h-[200px] md:h-[220px] xl:h-[300px] rounded-t-3xl" />
+                            <div className="h-[54px]" />
+                          </div>
+                          <div className="p-4 sm:p-5 flex flex-col gap-2 rounded-b-3xl w-full">
+                            <Skeleton className="h-4 w-24" />
+                            <Skeleton className="h-5 w-3/4" />
+                            <div className="py-1">
+                              <Skeleton className="h-1.5 w-full" />
+                            </div>
+                            <Skeleton className="h-4 w-40" />
+                            <Skeleton className="h-10 w-40 rounded-full mt-2" />
+                          </div>
+                        </div>
+                      </div>
+                    ))
+                  : products.map((p, index) => (
+                      <ScrollRevealCard
+                        key={p.id}
+                        delay={index * 0.1}
+                        duration={0.6}
+                      >
+                        <Link href={`/product/${p.slug}`}>
+                          <ProductCard
+                            brand={p.brand}
+                            productName={p.productName}
+                            participation={p.participation}
+                            image={p.image}
+                            imageAlt={p.imageAlt}
+                            rate={p.rate}
+                            bgColor={p.bgColor!}
+                            hex={p.hex!}
+                            time={p.time!}
+                          />
+                        </Link>
+                      </ScrollRevealCard>
+                    ))}
               </div>
             </div>
           </div>
