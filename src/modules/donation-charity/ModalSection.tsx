8ef2e7778ecbd7e987446d1ac3a4fd7743672ecb
Noa <nguyenduynoa198@gmail.com>
2025-09-12 18:00:44 +0700

    fix: animate domation

--- src/modules/donation-charity/ModalSection.tsx
+++ src/modules/donation-charity/ModalSection.tsx
@@ -53,10 +53,11 @@ const ModalSection = () => {
       </div>
       <div className="flex flex-col justify-center items-center gap-4 2xl:gap-6 w-full">
         <div className="flex justify-center items-center gap-4 2xl:gap-8 w-full">
-          <div className="relative size-[250px] bg-indigo-500 rounded-full flex-shrink-0 flex flex-col items-center justify-center p-6 overflow-visible">
-            <span className="absolute inset-0 m-6 rounded-full bg-indigo-500 opacity-20 animate-[ping_2s_cubic-bezier(0,0,0.2,1)_infinite]" />
+          <div className="relative size-[200px] 2xl:size-[250px] bg-indigo-500 rounded-full flex-shrink-0 flex flex-col items-center justify-center p-6 overflow-visible">
+            <span className="absolute z-[-1] inset-0 m-8 rounded-full bg-indigo-500 opacity-20 animate-[ping_4s_cubic-bezier(0,0,0.2,1)_infinite]" />
+            <span className="absolute z-[-1] inset-0 m-11 rounded-full bg-indigo-500 opacity-60 animate-[ping_4s_cubic-bezier(0,0,0.2,1)_infinite]" />
             <MoneySavingIcon />
-            <p className="text-xl font-semibold text-white">Funding Sources</p>
+            <p className="text-lg 2xl:text-xl font-semibold text-white text-center">Funding Sources</p>
           </div>
 
           <Line />
@@ -69,27 +70,29 @@ const ModalSection = () => {
             >
               <g clipPath="url(#clip0_44_469)">
                 <path
-                  d="M184.29 6.75012C209.65 1.64012 235.27 -1.43996 261.12 0.670044C313.4 4.95006 359.81 23.9004 399.21 58.7404C439.71 94.5503 465.06 139.6 477.46 191.93C482.46 213.05 484.66 234.7 481.21 256.28C475.75 290.43 455.5 312.29 422.73 322.61C416.43 324.59 409.78 325.43 403.31 326.88C399.83 327.66 397.99 326.259 396.23 323.19C376.77 289.34 357.15 255.58 337.57 221.81C305.4 166.32 273.59 110.62 240.9 55.4503C227.17 32.2703 205.45 19.1303 179.35 13.1603C176.7 12.5503 174.01 12.1102 171.33 11.5802C171.29 11.1402 171.25 10.6796 171.2 10.2296C175.56 9.05963 179.87 7.6401 184.29 6.75012Z"
-                  fill="#FE6BBA"
-                >
-                  <animate
-                    attributeName="fill"
-                    values="#FE6BBA; #FFD1E9; #FE6BBA"
-                    dur="3s"
-                    begin="0s"
-                    repeatCount="indefinite"
-                  />
-                </path>
-                <path
                   d="M76.52 417.44C69.93 410.04 63.46 403.21 57.48 395.99C32.26 365.58 14.29 331.53 5.77998 292.73C-5.48002 241.33 -0.310019 191.49 21.56 143.83C43.08 96.9301 74.77 58.4301 120.27 32.6201C160.34 9.89012 201.55 17.7001 231.12 53.1401C237.74 61.0801 237.71 61.0601 232.62 69.9201C181.82 158.2 130.67 246.28 80.47 334.9C66.87 358.91 67.11 385.17 75.88 411.24C76.35 412.64 76.86 414.04 77.28 415.46C77.36 415.69 77.08 416.04 76.52 417.44Z"
                   fill="#FEA6D6"
                 >
                   <animate
+                    id="anim1"
                     attributeName="fill"
                     values="#FEA6D6; #FFE3F1; #FEA6D6"
                     dur="3s"
-                    begin="0.5s"
-                    repeatCount="indefinite"
+                    begin="0s;anim3.end"
+                    repeatCount="1"
+                  />
+                </path>
+                <path
+                  d="M184.29 6.75012C209.65 1.64012 235.27 -1.43996 261.12 0.670044C313.4 4.95006 359.81 23.9004 399.21 58.7404C439.71 94.5503 465.06 139.6 477.46 191.93C482.46 213.05 484.66 234.7 481.21 256.28C475.75 290.43 455.5 312.29 422.73 322.61C416.43 324.59 409.78 325.43 403.31 326.88C399.83 327.66 397.99 326.259 396.23 323.19C376.77 289.34 357.15 255.58 337.57 221.81C305.4 166.32 273.59 110.62 240.9 55.4503C227.17 32.2703 205.45 19.1303 179.35 13.1603C176.7 12.5503 174.01 12.1102 171.33 11.5802C171.29 11.1402 171.25 10.6796 171.2 10.2296C175.56 9.05963 179.87 7.6401 184.29 6.75012Z"
+                  fill="#FE6BBA"
+                >
+                  <animate
+                    id="anim2"
+                    attributeName="fill"
+                    values="#FE6BBA; #FFD1E9; #FE6BBA"
+                    dur="3s"
+                    begin="anim1.end"
+                    repeatCount="1"
                   />
                 </path>
                 <path
@@ -97,11 +100,12 @@ const ModalSection = () => {
                   fill="#FE89C8"
                 >
                   <animate
+                    id="anim3"
                     attributeName="fill"
                     values="#FE89C8; #FFC8E6; #FE89C8"
                     dur="3s"
-                    begin="1s"
-                    repeatCount="indefinite"
+                    begin="anim2.end"
+                    repeatCount="1"
                   />
                 </path>
               </g>
@@ -141,10 +145,11 @@ const ModalSection = () => {
             </div>
           </div>
           <Line />
-          <div className="relative size-[250px] bg-orange-600 rounded-full flex-shrink-0 flex flex-col items-center justify-center p-6 overflow-visible">
-            <span className="absolute inset-0 m-6 rounded-full bg-orange-600 opacity-20 animate-[ping_2s_cubic-bezier(0,0,0.2,1)_infinite]" />
+          <div className="relative size-[200px] 2xl:size-[250px] bg-orange-600 rounded-full flex-shrink-0 flex flex-col items-center justify-center p-6 overflow-visible">
+            <span className="absolute z-[-1] inset-0 m-8 rounded-full bg-orange-600 opacity-20 animate-[ping_4s_cubic-bezier(0,0,0.2,1)_infinite]" />
+            <span className="absolute z-[-1] inset-0 m-11 rounded-full bg-orange-600 opacity-60 animate-[ping_4s_cubic-bezier(0,0,0.2,1)_infinite]" />
             <WomanIcon />
-            <p className="text-xl font-semibold text-white">Funding Sources</p>
+            <p className="text-lg 2xl:text-xl font-semibold text-white text-center">Funding Sources</p>
           </div>
         </div>
         <LineHorizontal />
