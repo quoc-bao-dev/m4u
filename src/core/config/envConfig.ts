3bcf032c5b54234d4381aa70201c592ab25f769d
Quoc Bao <quocbaodev1102@gmail.com>
2025-09-12 10:15:15 +0700

    Add functionality to fill form from saved API call history
    - Implemented handleFillFromHistory function to populate form fields with data from selected saved API calls.
    - Added a "Fill to Form" button in the UI to trigger the fill action.
    - Enhanced state management for parameters, request body, and domain selection based on saved calls.

--- src/core/config/envConfig.ts
+++ src/core/config/envConfig.ts
@@ -1,6 +1,6 @@
 export const envConfig = {
   baseUrl:
     (process.env.NEXT_PUBLIC_BASE_API_URL as string) ??
-    'http://192.168.1.178:8883/api',
+    'http://192.168.1.199:8883/api/',
   apiTimeout: parseInt(process.env.NEXT_PUBLIC_API_TIMEOUT || '10000'),
 }
