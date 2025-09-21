7f952a0d5aa362f72f79fb05ddbfbcc5f286b637
Quoc Bao <quocbaodev1102@gmail.com>
2025-09-21 13:41:12 +0700

    feat: add account preferences localization and update UserInfo component

--- src/modules/personal/components/user-info/UserInfo.tsx
+++ src/modules/personal/components/user-info/UserInfo.tsx
@@ -1,11 +1,20 @@
+'use client'
+
+import { useTranslations } from 'next-intl'
 import UserGeneralForm from './UserGeneralForm'
 import UserSecurityForm from './UserSecurityForm'
 
 const UserInfo = () => {
+  const t = useTranslations()
+
   return (
     <div>
       <div className="py-2">
-        <h1 className="text-2xl font-bold">Account preferences</h1>
+        <h1 className="text-2xl font-bold">
+          {t('personal.accountPreferences', {
+            defaultValue: 'Account preferences',
+          })}
+        </h1>
       </div>
       <UserGeneralForm />
       <UserSecurityForm />
