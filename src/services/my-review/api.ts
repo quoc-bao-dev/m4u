3ab08489143522224c1220e586342f8b15a4de65
Quoc Bao <quocbaodev1102@gmail.com>
2025-09-23 17:10:31 +0700

    feat: implement infinite scrolling and enhance review filtering in MyReviewTable component

--- src/services/my-review/api.ts
+++ src/services/my-review/api.ts
@@ -3,8 +3,34 @@ import axiosInstance from '@/core/http/axiosInstance'
 import { MyReviewResponse } from './type'
 
 export const apiMyReview = {
-  getMyReview: () =>
-    axiosInstance.get<MyReviewResponse>('get_my_review', {
-      baseURL: envConfig.adminUrl,
-    }),
+  getMyReview: ({
+    activeTab,
+    searchQuery,
+    dateStart,
+    dateEnd,
+    per_page,
+    current_page,
+  }: {
+    activeTab?: string
+    searchQuery?: string
+    dateStart?: string
+    dateEnd?: string
+    per_page?: number
+    current_page?: number
+  }) =>
+    axiosInstance.get<MyReviewResponse>(
+      'get_my_review',
+
+      {
+        baseURL: envConfig.adminUrl,
+        params: {
+          search: searchQuery,
+          status: activeTab,
+          date_start_sign_up: dateStart,
+          date_end_sign_up: dateEnd,
+          per_page,
+          current_page,
+        },
+      }
+    ),
 }
