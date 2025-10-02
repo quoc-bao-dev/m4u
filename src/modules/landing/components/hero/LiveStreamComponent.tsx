0de952617a830ea54649d3b72cfe238ed5e0fda7
Noa <nguyenduynoa198@gmail.com>
2025-10-02 11:33:37 +0700

    style: refine layout and responsive sizing in AvatarStack, LiveStreamComponent, and ReviewerAvatar components

--- src/modules/landing/components/hero/LiveStreamComponent.tsx
+++ src/modules/landing/components/hero/LiveStreamComponent.tsx
@@ -10,22 +10,25 @@ const LiveStreamComponent = () => {
     <>
       <div className="bg-[#FFF0F8] py-2 xl:py-4 2xl:py-8">
         <Container>
-          <div className="flex flex-col md:flex-row items-center justify-between gap-2 md:gap-6">
+          <div className="flex flex-row items-center justify-between gap-2 md:gap-6">
             {/* Live Stream Badge */}
-            <div className="flex items-center md:justify-start justify-center w-full bg">
+            <div className="flex items-center md:justify-start justify-center ">
               <LiveStreamTracking />
             </div>
 
             {/* Members Section */}
-            <div className="flex flex-col md:flex-row items-center gap-2">
+            <div className="flex flex-col md:flex-row items-end gap-2">
               {/* Member Info */}
-              <div className="text-center md:text-right">
+              <div className="text-right">
                 <p className="text-gray-900 text-sm sm:text-lg md:text-lg font-bold truncate">
-                  {t('membersOnline')}
+                  {t('membersOnline')}{' '}
+                  <span className="lg:hidden text-[#F5222D] font-bold">
+                    69+ {t('singleMoms')}
+                  </span>{' '}
                 </p>
                 <p className="text-xs sm:text-sm lg:text-base ">
-                  <span className="text-[#F5222D] font-bold">
-                    69+ {t('singleMoms')}
+                  <span className="hidden lg:block text-[#F5222D] font-bold">
+                    69+ {t('singleMoms')}<br/>
                   </span>{' '}
                   <span className="text-[#4B5563] truncate">
                     {t('participatingInGroup')}
