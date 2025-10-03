988894f5a8e496d25ba24429f19590e314ca6a58
Quoc Bao <quocbaodev1102@gmail.com>
2025-10-03 10:52:02 +0700

    refactor: update layout component to handle dynamic scrolling behavior and improve code organization

--- src/app/[locale]/(personal)/layout.tsx
+++ src/app/[locale]/(personal)/layout.tsx
@@ -1,15 +1,17 @@
 'use client'
 
 import { Container } from '@/core/components'
-import { AuthGuard, AuthGuardSkeleton } from '@/core/guard'
 import { IMAGES } from '@/core/constants/IMAGES'
+import { AuthGuard, AuthGuardSkeleton } from '@/core/guard'
 import { PersonalSidebar } from '@/modules/personal'
-import { PropsWithChildren } from 'react'
 import { usePathname } from 'next/navigation'
+import { PropsWithChildren } from 'react'
+
+const configPath = ['my-reviews']
 
 const Layout = ({ children }: PropsWithChildren) => {
   const pathname = usePathname()
-  const isReferralProgram = pathname?.includes('referral-program')
+  const isScroll = configPath.some((path) => pathname?.includes(path))
   return (
     <AuthGuard redirectTo="/" fallback={<AuthGuardSkeleton />}>
       <div className="py-[96px] relative min-h-screen">
@@ -28,13 +30,17 @@ const Layout = ({ children }: PropsWithChildren) => {
               </div>
             </aside>
             <section className="lg:col-span-8 xl:col-span-9">
-              <div className={`lg:p-5 lg:bg-white lg:rounded-2xl lg:shadow-[0px_4px_24px_0px_#0000000F] ${
-                isReferralProgram
-                  ? 'lg:min-h-[calc(100vh-162px)]'
-                  : 'lg:h-[calc(100vh-162px)]'
-              } h-full relative overflow-hidden flex flex-col min-h-0`}>
+              <div
+                className={`lg:p-5 lg:bg-white lg:rounded-2xl lg:shadow-[0px_4px_24px_0px_#0000000F] ${
+                  isScroll
+                    ? 'lg:h-[calc(100vh-162px)]'
+                    : 'lg:min-h-[calc(100vh-162px)]'
+                } h-full relative overflow-hidden flex flex-col min-h-0`}
+              >
                 {/* Main table content area - children */}
-                <div className="relative z-10 flex-1 min-h-0 flex flex-col">{children} </div>
+                <div className="relative z-10 flex-1 min-h-0 flex flex-col">
+                  {children}{' '}
+                </div>
 
                 <div className="hidden lg:block absolute -bottom-6 left-0 right-0 z-8 translate-y-3/4 scale-x-[-1.8] overflow-hidden">
                   <img
