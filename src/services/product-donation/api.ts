be7b0111bd2de39ca5e4f5e1dbfa3453bc3495c0
Quoc Bao <quocbaodev1102@gmail.com>
2025-10-13 17:02:20 +0700

    fix: update API parameter for product donation list retrieval
    - Changed the 'is_contribute' parameter from 2 to 1 in the product donation API call to adjust the filtering criteria for fetching donation lists.

--- src/services/product-donation/api.ts
+++ src/services/product-donation/api.ts
@@ -6,6 +6,6 @@ export const productDonationApi = {
   getDonationList: ({ _local }: { _local?: string }) =>
     axiosInstance.get<ProductListResponse>('products/getList', {
       baseURL: envConfig.adminUrl,
-      params: { _local, is_contribute: 2 },
+      params: { _local, is_contribute: 1 },
     }),
 }
